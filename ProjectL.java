95e1f22497609fc2778a6dc240f80a469137eabf
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-02 23:45:56 +0530

    Initial Commit

---
+++ ProjectL.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ProjectL {
+    public static void main(String[] args) {
+        Scanner hr = new Scanner(System.in);
+        String F1 = hr.next(), S1 = hr.next(), F2 = hr.next(), S2 = hr.next();
+        String result = S1.equals(S2) ? "ARE Brothers" : "NOT";
+        System.out.println(result);
+    }
+}
