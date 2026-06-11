0d32de8080cc084b7fd40f6e7dcdd5bff15f247f
Himanshu Ranjan <himanshuranjan.cse@gmail.com>
2026-06-11 12:39:29 +0530

    Resolved typo

--- ProjectL.java
+++ ProjectL.java
@@ -4,7 +4,7 @@ public class ProjectL {
     public static void main(String[] args) {
         Scanner hr = new Scanner(System.in);
         String F1 = hr.next(), S1 = hr.next(), F2 = hr.next(), S2 = hr.next();
-        String result = S1.equals(S2) ? "ARE Brothers" : "NOT";
+        String result = S1.equals(S2) ? "ARE BROTHERS" : "NOT";
         System.out.println(result);
 
         hr.close();
